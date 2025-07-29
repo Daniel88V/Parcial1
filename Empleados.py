de7d521722df532b7ce4afbe1ad3c928d0450cf8
Daniel88V <daniel12velo@gmail.com>
2025-07-28 18:35:17 -0600

    Parcial 1

--- Empleados.py
+++ Empleados.py
@@ -14,3 +14,33 @@ def agregar_empleados():
         while True:
             try:
                 antiguedad = int(input(f"Ingrese los años que el empleado lleva trabajando en la empresa: "))
+                if antiguedad < 0:
+                    print("Error, los años no pueden ser negativos")
+                    continue
+                break
+            except ValueError:
+                print("Error, por favor ingresa un valor valido")
+        while True:
+            try:
+                puntualidad = int(input(f"Ingrese la calificacion en la puntualidad del empleado: "))
+                if puntualidad < 0:
+                    print("Error, la calificacion no puede ser negativa")
+                    continue
+                if puntualidad > 10:
+                    print("Error, la calificacion maxima es 10")
+                    continue
+                break
+            except ValueError:
+                print("Error, por favor ingresa un valor valido")
+        while True:
+            try:
+                trabajo_equipo = int(input(f"Ingrese la calificacion de trabajo equipo del empleado: "))
+                if trabajo_equipo < 0:
+                    print("Error, la calificacion no puede ser negativa")
+                    continue
+                if trabajo_equipo > 10:
+                    print("Error, la calificacion maxima es 10")
+                    continue
+                break
+            except ValueError:
+                print("Error, por favor ingresa un valor valido")
